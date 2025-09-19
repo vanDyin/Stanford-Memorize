8b06f70b439d6832eaf1b6a43aa744a97c4024d3
vanDyin <vycheslavoo2006@yandex.com>
2025-09-19 23:55:08 +0300

    Add flying numbers

--- Memorize/ViewMemorizeGame.swift
+++ Memorize/ViewMemorizeGame.swift
@@ -33,20 +33,27 @@ struct ViewMemorizeGame: View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
             CardView(viewModel: viewModel, card)
                 .padding(spacing)
-                .overlay {
-                    FlyingNumber(number: scoreChange(causedBy: card))
-                }
+                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
                 .onTapGesture {
-                    withAnimation {
-                        viewModel.choose(card)
-                    }
-
+                    choose(card)
                 }
         }
     }
     
+    private func choose(_ card: Card) {
+        withAnimation {
+            let scoreBeforeChoosing = viewModel.score
+            viewModel.choose(card)
+            let scoreChange = viewModel.score - scoreBeforeChoosing
+            lastScoreChange = (scoreChange, causedByCardId: card.id)
+        }
+    }
+    
+    @State private var lastScoreChange = (0, causedByCardId: UUID())
     private func scoreChange(causedBy card: Card) -> Int {
-        return 0
+        let (amount, id) = lastScoreChange
+        return card.id == id ? amount : 0
     }
     
     var title: some View {
